bce52c9a8dfdb100687cc8e2a0df71082047fd4c
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-06-08 16:18:32 -0300

    feat: Aulas 5 a 8

--- src/com/estruturadados/vetor/Vetor.java
+++ src/com/estruturadados/vetor/Vetor.java
@@ -34,6 +34,8 @@ public class Vetor {
 	}*/
 
 	public boolean adiciona(String elemento) {
+		
+		this.aumentaCapacidade();
 
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
@@ -41,6 +43,49 @@ public class Vetor {
 			return true;
 		} 
 		return false;
+	}
+	
+	public boolean adiciona(int posicao, String elemento) {
+		
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+		
+		this.aumentaCapacidade();
+		
+		for (int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+		
+		return true;
+	}
+	
+	private void aumentaCapacidade() {
+		if (this.tamanho == this.elementos.length) {
+			String[] elementosNovos = new String[this.elementos.length * 2];
+			for (int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	
+	public String busca(int posicao) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+		return this.elementos[posicao];
+	}
+	
+	public int busca(String elemento) {
+		for (int i = 0; i < this.tamanho; i++) {
+			if (this.elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	public int tamanho() {
