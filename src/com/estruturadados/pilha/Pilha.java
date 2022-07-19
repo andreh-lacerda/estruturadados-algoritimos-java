8dbaeabe7cc2326cb77b88c29ea69abef34e7e82
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-07-19 19:31:44 -0300

    feat: Aula 13 Pilhas

---
+++ src/com/estruturadados/pilha/Pilha.java
@@ -0,0 +1,15 @@
+package com.estruturadados.pilha;
+
+import com.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+	
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+
+}
---
+++ src/com/estruturadados/pilha/teste/Aula13.java
@@ -0,0 +1,16 @@
+package com.estruturadados.pilha.teste;
+
+import com.estruturadados.pilha.Pilha;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+		
+		Pilha<Integer> pilha = new Pilha<Integer>();
+		
+		System.out.println(pilha);
+		System.out.println(pilha.tamanho());
+
+	}
+
+}
